c82939e44e7c57d250bc01797a7f407c1354c8f8
Simone Celia <simone.celia@eng.it>
2024-06-26 18:45:31 +0200

    1.1.0

--- Constants.py
+++ Constants.py
@@ -17,3 +17,17 @@ else:
 
 URL = config.get(APPLICATION, "url")
 TEXT = config.get(APPLICATION, "text")
+SLEEP = config.get(APPLICATION, "sleep")
+#
+MAIL_APP_NAME = config.get(APPLICATION, "mail_app_name")
+MAIL_APP_PASS = config.get(APPLICATION, "mail_app_pass")
+#
+SMTP_SERVER = config.get(APPLICATION, "smtp_server")
+SMTP_PORT = config.get(APPLICATION, "smtp_port")
+#
+MAIL_FROM = config.get(APPLICATION, "mail_from")
+MAIL_TO = config.get(APPLICATION, "mail_to")
+MAIL_SUBJECT_DOWN = config.get(APPLICATION, "mail_subject_down")
+MAIL_BODY_DOWN = config.get(APPLICATION, "mail_body_down")
+MAIL_SUBJECT_UP = config.get(APPLICATION, "mail_subject_up")
+MAIL_BODY_UP = config.get(APPLICATION, "mail_body_up")
